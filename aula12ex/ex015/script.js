ef317aed47b83b39ae0ead21229eed2dc03ecb97
LopesPJL <guilhermelopespjl@gmail.com>
2024-12-04 11:11:51 -0300

    atribuindo img

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,6 +9,41 @@ function Verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada ${idade}`
+       var genero = '' 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+       if (fsex[0].checked) {
+        genero = 'Homem'
+        if (idade >= 0 && idade < 10) {
+            //criança
+            img.setAttribute('src', 'foto-crianca-m.png')
+        } else if (idade < 21) {
+            //jovem 
+            img.setAttribute('src', 'foto-joven-m.png')
+        } else if (idade < 50) {
+            //aduto
+            img.setAttribute('src', 'foto-aduto-m.png')
+        } else {
+            //idoso 
+            img.setAttribute('src', 'foto-idoso-m.png')
+        }
+       } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >= 0 && idade < 10) {
+            //criança 
+            img.setAttribute('src', 'foto-crianca-f.png')
+        } else if (idade < 21) {
+            //jovem
+            img.setAttribute('src', 'foto-joven-f.png')
+        } else if (idade < 50) {
+            //aduto 
+            img.setAttribute('src', 'foto-aduto-f.png')
+        } else {
+            //idoso 
+            img.setAttribute('src', 'foto-idosa-f.png')
+        }
+       }
+       res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+       res.appendChild(img)
     }
 }
