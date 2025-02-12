ee2a88e081b5aa7156056c3f134845ca54508752
LopesPJL <guilhermelopespjl@gmail.com>
2025-02-12 10:58:51 -0300

    exercicio de tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,17 +1,19 @@
 function Contar() {
-    var ta = document.getElementById('txtt')
-    var res = document.getElementById('res')
-
-    if (ta.value.length == 0){
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0){
         res.innerHTML = 'Impossível contar'
         window.alert('Error')
     } else {
-        var t = Number(ta.value)
-
-        var c = t 
-        while(c <= 10) {
-            console.log(c * 1)
-
+        var t = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${t} x ${c} = ${t*c}`
+            tab.appendChild(item) 
+            c++
         }
     }
 }
